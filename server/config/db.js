f27aa48c83472ba54b9315736e464427d1cca9b0
samreen41 <mdsamreenmohammad@gmail.com>
2026-06-22 19:17:04 +0530

    db.js changes

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connecting to MongoDB...");
+    console.log(process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Failed");
-    console.error(err.message);
-    process.exit(1);
+    console.error(err);
   }
 };
 
